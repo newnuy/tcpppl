e8804e5a847e1afab364db6865b9a30baba1f9c4
newnuy <newnuy@newnuy.com>
2012-11-10 11:24:09 +0800

    add 02 under chapter04

---
+++ chapter04/02/02.cc
@@ -0,0 +1,26 @@
+#include <string>
+
+using namespace std;
+
+extern char ch;
+extern string s;
+extern int count;
+extern const double pi;
+int error_number;
+
+extern const char *name;
+extern const char *season[];
+
+struct Date;
+int day(Date *p);
+double sqrt(double num) { return -1; }
+template<class T> T abs(T a);
+
+// typedef ?
+struct User { string name; bool sex; };
+// enum ?
+// namespace ?
+
+int main()
+{
+}
