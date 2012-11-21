2aa27855b80dee3c77db5bfbbccfea308125f7af
newnuy <newnuy@newnuy.com>
2012-11-21 13:54:26 +0800

    add 05 under chapter05

---
+++ chapter05/05/05.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char str[] = "a short string";
+    cout << sizeof(str) << endl;
+    cout << strlen(str) << endl;
+
+    return 0;
+}
