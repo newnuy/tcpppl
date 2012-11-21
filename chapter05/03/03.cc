8c4f34458bc092e6d5981d33d97b9859a5869936
newnuy <newnuy@newnuy.com>
2012-11-21 13:30:48 +0800

    add 03 under chapter05

---
+++ chapter05/03/03.cc
@@ -0,0 +1,13 @@
+typedef unsigned char uchar;
+typedef const unsigned char cuchar;
+typedef int * pint;
+typedef char ** ppchar;
+typedef char **ps;
+typedef int *spint[7];
+typedef int *(*pspint)[7];
+typedef int *s8[8][7];
+
+int main()
+{
+    return 0;
+}
